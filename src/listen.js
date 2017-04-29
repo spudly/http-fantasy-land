f3d5e7e547ddfe9aa77c3808692ed078889f40e8
Stephen Sorensen <shuoink@gmail.com>
2017-04-28 21:34:51 -0400

    http support - fixes #9

--- src/listen.js
+++ src/listen.js
@@ -1,11 +1,13 @@
 import http from 'http';
+import https from 'https';
 import createRequest from './utils/createRequest';
 import createResponse from './utils/createResponse';
 import sendResponse from './utils/sendResponse';
 import sendFailureResponse from './utils/sendFailureResponse';
 
-const listen = async (reducer, {port}) => {
-  const server = http.createServer(async (nativeRequest, nativeResponse) => {
+const listen = async (reducer, options) => {
+  const {port, host, backlog, ssl, ...httpsOptions} = options;
+  const requestListener = async (nativeRequest, nativeResponse) => {
     try {
       const request = createRequest(nativeRequest);
       const response = await reducer(createResponse(), request);
@@ -13,10 +15,13 @@ const listen = async (reducer, {port}) => {
     } catch (error) {
       sendFailureResponse(nativeResponse, error);
     }
-  });
+  };
+  const server = ssl
+    ? https.createServer(httpsOptions, requestListener)
+    : http.createServer(requestListener);
 
   await new Promise((resolve, reject) => {
-    server.listen(port).on('listening', resolve).once('error', reject);
+    server.listen(port, host, backlog).on('listening', resolve).once('error', reject);
   });
 
   return () => server.close();
