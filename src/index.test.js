c359bb36febdc1891e9e3e8638ab979d3caba425
Stephen Sorensen <shuoink@gmail.com>
2017-05-09 21:01:39 -0400

    create a function for each http method

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,15 @@
 import * as exports from './index.js';
 import map from 'ramda/src/map';
+import {METHODS} from 'http';
 
 test('exports functions', () => {
   const exportTypes = map(value => typeof value, exports);
   expect(exportTypes).toMatchSnapshot();
+});
+
+test('exports a function for each method in http.METHODS', () => {
+  METHODS.forEach(method => {
+    const name = method.toLowerCase().replace(/-/g, '');
+    expect(exports).toHaveProperty(name);
+  });
 });
